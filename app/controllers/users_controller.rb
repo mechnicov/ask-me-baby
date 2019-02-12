0cc419bfbf6750eedcced87274a3ae93073dd43f
Edem <keltikan@gmail.com>
2019-02-12 21:12:11 +0300

    Questions count was added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Bob Marley',
-        username: 'rastaman',
-        avatar_url: 'http://zano.ru/avatar/images/100j1209.jpg'
-      ),
-      User.new(id: 2, name: 'Антон', username: 'chehov')
-    ]
+    @users = User.all
   end
 
   def new
@@ -18,17 +10,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.new(
-      name: 'Bob Marley',
-      username: 'rastaman',
-      avatar_url: 'http://zano.ru/avatar/images/100j1209.jpg'
-    )
-
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('11.02.2019')),
-      Question.new(text: 'Что случилось?', created_at: Date.parse('12.02.2019'))
-    ]
-
+    @user = User.find(params[:id])
+    @questions = @user.questions
     @new_question = Question.new
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,22 @@ module ApplicationHelper
       asset_url 'avatar.png'
     end
   end
+
+  def questions_qty(user)
+    qty = user.questions.count
+
+    remainder = qty % 100
+    if remainder >= 11 && remainder <= 14
+      return "#{qty} вопросов"
+    end
+
+    remainder = qty % 10
+    if remainder == 1
+      "#{qty} вопрос"
+    elsif remainder >=2 && remainder <= 4
+      "#{qty} вопроса"
+    else
+      "#{qty} вопросов"
+    end
+  end
 end
