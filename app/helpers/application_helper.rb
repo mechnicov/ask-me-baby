9738c38fb1e3c36e0a2b005280a3a83d0e633cb7
Edem <keltikan@gmail.com>
2019-02-18 00:14:49 +0300

    Questions added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,8 @@ module ApplicationHelper
   def question_time(question)
     l(question.created_at, format: '%-d %B %Y %k:%M')
   end
+
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, except: :destroy
   resources :sessions, only: [:new, :create, :destroy]
-  resources :questions
+  resources :questions, except: [:show, :new, :index]
   get 'sign_up', to: 'users#new'
   get 'log_out', to: 'sessions#destroy'
   get 'log_in', to: 'sessions#new'
