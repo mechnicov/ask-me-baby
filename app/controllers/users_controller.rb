7d837a2848f08b726ebeb74e769b302d973ea693
Edem <keltikan@gmail.com>
2019-02-18 04:43:46 +0300

    Create improved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'Регистрация прошла успешно'
     else
       render :new
     end
