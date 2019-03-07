3d5dc6744db552c9af29ae0b160b2fc8683fbaaa
Edem <keltikan@gmail.com>
2019-03-07 14:36:50 +0300

    View improvement

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
   attr_accessor :password
 
-  has_many :questions
+  has_many :questions, dependent: :destroy
   validates :username, presence: true, uniqueness: true,
                        length: { maximum: MAX_NAME_LENGTH },
                        format: { with: VALID_USERNAME_REGEX }
