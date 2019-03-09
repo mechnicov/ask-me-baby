9566ff6c729cdb17f7645e4e5645fed73ed78c00
Edem <keltikan@gmail.com>
2019-03-10 01:03:08 +0300

    Routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
   resources :hashtags, only: :show, param: :name
-  get 'sign_up', to: 'users#new'
-  get 'log_out', to: 'sessions#destroy'
-  get 'log_in', to: 'sessions#new'
 end
